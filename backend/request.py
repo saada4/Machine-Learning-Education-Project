928a6a90d4e3a67e8b94c22537fa286ce2b08996
zoezheng126 <44520561+zoezheng126@users.noreply.github.com>
2024-03-19 15:59:46 -0500

    Update request.py

--- backend/request.py
+++ backend/request.py
@@ -4,10 +4,23 @@ import json
 # URL of the Flask app endpoint
 url = 'http://130.126.139.253:3000/get_result'
 
+# user give class categories, at least 5
+name_list = [
+    'fighting',
+    'playing',
+    'running',
+    'jogging',
+    'reading',
+    'writing'
+]
+
 # Data to be sent to the API
 data = {
-    'video_url': 'http://130.126.139.253:5000/cat.mp4',
-    'app_id': '123'
+    'video_url': 'http://130.126.139.253:8080/cat.mp4',
+    'app_id': '123',
+    'name_list': name_list,
+    'filename': 'cat.mp4',
+    'label': 'fighting'
 }
 
 response = requests.post(url, json=data)
